9ea593f1dabbf19372a905d862ca808ab45f6b0a
suika0528a-code <suika0528a@gmail.com>
2026-03-15 11:35:28 +0800

    Add files via upload

---
+++ line_stock_bot.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import yfinance as yf
+
+app = Flask(__name__)
+
+LINE_CHANNEL_ACCESS_TOKEN = "BPQ/+EOBTi0a5fWsab/05yvB8J8v4jsh3zqjk2TqnSoMJ5CsJxU2+RTGKlQx0FndpaX1nkj88rDh9HUk0mXCvK znM3sTGM9k6upXohJb/+JtLYzFboHjcT41gIldo8TNka3g0m8jfe/dgZuU8ll8tAdB04t89/1O/w1cDnyilFU="
+LINE_CHANNEL_SECRET = "23cd049315c1c76fc3c115445fe1f650"
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+
+    text = event.message.text.upper()
+
+    try:
+        stock = yf.Ticker(text)
+        price = stock.info["regularMarketPrice"]
+
+        msg = f"{text} 目前價格: ${price}"
+
+    except:
+        msg = "請輸入股票代碼，例如 NVDA"
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=msg)
+    )
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
