7377d06df6aee444639043e4ecfbfe06e7b83975
suika0528a-code <suika0528a@gmail.com>
2026-03-15 11:56:42 +0800

    Update line_stock_bot.py

--- line_stock_bot.py
+++ line_stock_bot.py
@@ -6,7 +6,7 @@ import yfinance as yf
 app = Flask(__name__)
 
 LINE_CHANNEL_ACCESS_TOKEN ="9M9gWuDL+9swPRQtYnGcAfXFwwy1rwlI2b9gBXBhmKv/5yXkdvAMWKCzptxJu0E9paX1nkj88rDh9HUk0mXCvKznM3sTGM9k6upXohJb/+I/civXiL3dU6dp/1Rpz2X+vPvAhMGj5PE5p0L1pCPR7gdB04t89/1O/w1cDnyilFU="
-LINE_CHANNEL_SECRET = "23cd049315c1c76fc3c115445fe1f650"
+LINE_CHANNEL_SECRET = "f7f7b714907d5efd93a29d5866c9593b"
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
